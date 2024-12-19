3d4dfad7a510eddad28813d5bef35f0997c70375
Rahmat Aramadhan Husni <53205828+RahmatAramadhan@users.noreply.github.com>
2024-12-19 08:57:15 +0700

    feat: update register connect to firestore

--- app/src/main/java/com/example/ta_papb/Register.kt
+++ app/src/main/java/com/example/ta_papb/Register.kt
@@ -11,12 +11,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
     private lateinit var btnRegister: Button
+    private lateinit var etName: EditText
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var etConfirmPass: EditText
@@ -40,10 +43,12 @@ class Register : AppCompatActivity() {
 
     private fun initFirebaseauth() {
         auth = Firebase.auth
+        firestore = FirebaseFirestore.getInstance()
     }
 
     private fun setupListeners() {
         btnLogin = findViewById(R.id.btn_login)
+        etName = findViewById(R.id.et_name)
         etEmail = findViewById(R.id.et_email)
         etPassword = findViewById(R.id.et_password)
         etConfirmPass = findViewById(R.id.et_confirmPassword)
@@ -54,18 +59,23 @@ class Register : AppCompatActivity() {
     }
 
     private fun handleRegistration() {
+        val name = etName.text.toString().trim()
         val email = etEmail.text.toString().trim()
         val password = etPassword.text.toString().trim()
         val confirmPassword = etConfirmPass.text.toString().trim()
 
-        if (!validateInput(email, password, confirmPassword)) {
+        if (!validateInput(name, email, password, confirmPassword)) {
             return
         }
-        registerUser(email, password)
+        registerUser(name, email, password)
     }
 
-    private fun validateInput(email: String, password: String, confirmPassword: String): Boolean {
+    private fun validateInput(name: String, email: String, password: String, confirmPassword: String): Boolean {
         return when {
+            name.isEmpty() -> {
+                showToast("Name cannot be empty")
+                false
+            }
             email.isEmpty() -> {
                 showToast("Email cannot be empty")
                 false
@@ -89,11 +99,26 @@ class Register : AppCompatActivity() {
             else -> true
         }
     }
-    private fun registerUser(email: String, password: String) {
+    private fun registerUser(name: String, email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    navigateLogin()
+                    val userId = auth.currentUser?.uid ?: return@addOnCompleteListener
+
+                    val userData = hashMapOf(
+                        "email" to email,
+                        "name" to name,
+                        "photoPath" to "profile_pictures/default.jpg"
+                    )
+
+                    firestore.collection("users").document(userId).set(userData)
+                        .addOnSuccessListener{
+                            showToast("registration successful! User data saved.")
+                            navigateLogin()
+                        }
+                        .addOnFailureListener{
+                            showToast("Failed to save user data: ${it.message}")
+                        }
                 } else {
                     showToast("Registration failed: ${task.exception?.message}")
                 }
@@ -111,6 +136,5 @@ class Register : AppCompatActivity() {
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-
 
 }
